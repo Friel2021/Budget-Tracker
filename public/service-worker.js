c734f7ffe9ea29aebfb16c75413e34923d5e21a8
friel2021 <friel2009@gmail.com>
2021-09-27 14:44:36 -0600

    updated db file caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/manifest.json",
+  "/manifest.webmanifest",
   "/db.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
